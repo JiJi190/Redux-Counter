1dabf5432273dd88cc149cd82dbc6a434163e31c
unknown <robertrey1234@gmail.com>
2019-07-24 19:51:15 -0500

    MVP

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,11 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => {this.props.increment(this.props.count)}}>
+                {console.log(this.props.count)}
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => {this.props.decrement(this.props.count)}}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
